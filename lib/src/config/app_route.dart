ec0b7c90415184d6ea5d134761504af4c12e6143
wisanu.pljk <wisanu.pljk@gmail.com>
2021-04-22 13:03:35 +0700

    complete

--- lib/src/config/app_route.dart
+++ lib/src/config/app_route.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:my_stock/src/pages/map/map_page.dart';
 import 'package:my_stock/src/pages/pages.dart';
 
 class AppRoute {
   static const homeRoute = "home";
   static const loginRoute = "login";
   static const managementRoute = "management";
+  static const mapRoute = "map";
 
   get route => _route;
 
   final _route = <String, WidgetBuilder> {
     homeRoute: (context) => HomePage(),
     loginRoute: (context) => LoginPage(),
-    managementRoute: (content) => ManagementPage()
+    managementRoute: (content) => ManagementPage(),
+    mapRoute: (content) => GoogleMapPage()
   };
 }
--- lib/src/constants/api.dart
+++ lib/src/constants/api.dart
@@ -1,5 +1,5 @@
 class API {
-  static const String BASE_URL = 'https://18bb266feb01.ngrok.io';
+  static const String BASE_URL = 'https://06cf3bb7333e.ngrok.io';
   // static const String BASE_URL = 'http://192.168.1.102:1150';
   static const String IMAGE_URL = '$BASE_URL/images';
   static const String PRODUCT = '/products';
